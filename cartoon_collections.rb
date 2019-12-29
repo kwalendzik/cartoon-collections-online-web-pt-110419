e3e3bef4ece01f07998e2ebfb227b8e72c2e85f7
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-29 19:21:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each{|c| puts "#{c}.upcase"}
+  planeteer_calls.each{|c| c.capitalize + '!"'}
 end
 
 def long_planeteer_calls# code an argument here
