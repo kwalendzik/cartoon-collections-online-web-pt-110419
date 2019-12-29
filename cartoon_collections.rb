8a1744a9839a12bdf5154c1b0b8d856e0ba4e575
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-29 19:16:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  i = 0
-  if i < planeteer_calls.length
-    planeteer_calls.map{|c| puts "#{c}.upcase"}
-    i += 1
+  planeteer_calls.map{|c| puts "#{c}.upcase"}
   end
 end
 
